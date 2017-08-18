1d94232de88501f570e41d7515f0453261186907
Romain <seite.romain@yahoo.com>
2017-08-18 16:19:55 +0200

    fix progressbar and refactoring key press

--- js/query.js
+++ js/query.js
@@ -30,6 +30,7 @@ $(document).ready(function () {
         clearInterval(interval);
         $("#time").text("00 : 00");
         $("title").text("Pomodoro");
+        $('.progress-bar').css('width', '0').attr('aria-valuenow', '0').attr('aria-valuemax', '100');
     });
 
     $("#300").click(function () {
@@ -55,17 +56,59 @@ $(document).ready(function () {
         progress = 0.066666;
         displayClearTime(minute, second);
     });
+    
+    // Key Binding
+    $(document).keydown(function (e) {
+        switch (e.which) {
+            case 13: // ENTER
+                if (set == 0) {
+                    clearInterval(interval);
+                    set = 1;
+                } else {
+                    interval = setInterval(addSecond, 1000);
+                    set = 0;
+                }
+                break;
 
-    $(document).keypress(function (e) {
-        if (e.which == 32) {
-            if (set == 0) {
-                clearInterval(interval);
+            case 32: // SPACE
                 set = 1;
-            } else {
-                interval = setInterval(addSecond, 1000);
-                set = 0;
-            }
+                time = 0;
+                minute = 0;
+                second = 0;
+                $("#start").text("Start");
+                clearInterval(interval);
+                $("#time").text("00 : 00");
+                $("title").text("Pomodoro");
+                $('.progress-bar').css('width', '0').attr('aria-valuenow', '0').attr('aria-valuemax', '100');
+                break;
+
+            case 49: // SHIFT + &
+                time = 300;
+                minute = time / 60;
+                second = 0;
+                progress = 0.3333333;
+                displayClearTime(minute, second);
+                break;
+
+            case 50: // SHIFT + é
+                time = 600;
+                minute = time / 60;
+                second = 0;
+                progress = 0.1666666;
+                displayClearTime(minute, second);
+                break;
+            case 51: // SHIFT + "
+                time = 1500;
+                minute = time / 60;
+                second = 0;
+                progress = 0.066666;
+                displayClearTime(minute, second);
+                break;
+
+            default:
+                return;
         }
+        e.preventDefault();
     });
 
 
@@ -86,8 +129,8 @@ function displayClearTime(m, s) {
 
 function addSecond() {
     i += progress;
-$('.progress-bar').css('width', i+'%').attr('aria-valuenow', i).attr('aria-valuemax', progress * time);
-    
+    $('.progress-bar').css('width', i + '%').attr('aria-valuenow', i).attr('aria-valuemax', progress * time);
+
     if (time != 0) {
         if (second == 0) {
             minute--;
