06678c0619ee8b47ca120d20911fc883338d80b6
Romain <seite.romain@yahoo.com>
2017-08-21 10:01:15 +0200

    fix progressbar, fix some little bug

--- js/query.js
+++ js/query.js
@@ -2,30 +2,28 @@ var time = 0;
 var minute = 0;
 var second = 0;
 var interval = 0;
-var set = 1;
+var set = "stop";
 var i = 0;
 var progress = 0;
 $(document).ready(function () {
     $("#start").click(function () {
-        if (time == 0) {
-            $("#time").text("00 : 00");
-        }
-        if (set == 0) {
+        if (set === "play") {
             $("#start").text("Play");
             clearInterval(interval);
-            set = 1;
+            set = "stop";
         } else {
             $("#start").text("Pause");
             interval = setInterval(addSecond, 1000);
-            set = 0;
+            set = "play";
         }
     });
 
     $("#reset").click(function () {
-        set = 1;
+        set = "stop";
         time = 0;
         minute = 0;
         second = 0;
+        i = 0;
         $("#start").text("Start");
         clearInterval(interval);
         $("#time").text("00 : 00");
@@ -34,6 +32,9 @@ $(document).ready(function () {
     });
 
     $("#300").click(function () {
+        if (time != 0) {
+            i = 0;
+        }
         time = 300;
         minute = time / 60;
         second = 0;
@@ -42,6 +43,9 @@ $(document).ready(function () {
     });
 
     $("#600").click(function () {
+        if (time != 0) {
+            i = 0;
+        }
         time = 600;
         minute = time / 60;
         second = 0;
@@ -50,31 +54,37 @@ $(document).ready(function () {
     });
 
     $("#1500").click(function () {
+        if (time != 0) {
+            i = 0;
+        }
         time = 1500;
         minute = time / 60;
         second = 0;
         progress = 0.066666;
         displayClearTime(minute, second);
     });
-    
+
     // Key Binding
     $(document).keydown(function (e) {
         switch (e.which) {
             case 13: // ENTER
-                if (set == 0) {
+                if (set === "play") {
+                    $("#start").text("Play");
                     clearInterval(interval);
-                    set = 1;
+                    set = "stop";
                 } else {
+                    $("#start").text("Pause");
                     interval = setInterval(addSecond, 1000);
-                    set = 0;
+                    set = "play";
                 }
                 break;
 
             case 32: // SPACE
-                set = 1;
+                set = "stop";
                 time = 0;
                 minute = 0;
                 second = 0;
+                i = 0;
                 $("#start").text("Start");
                 clearInterval(interval);
                 $("#time").text("00 : 00");
@@ -83,6 +93,9 @@ $(document).ready(function () {
                 break;
 
             case 49: // SHIFT + &
+                if (time != 0) {
+                    i = 0;
+                }
                 time = 300;
                 minute = time / 60;
                 second = 0;
@@ -91,13 +104,19 @@ $(document).ready(function () {
                 break;
 
             case 50: // SHIFT + é
+                if (time != 0) {
+                    i = 0;
+                }
                 time = 600;
                 minute = time / 60;
                 second = 0;
                 progress = 0.1666666;
                 displayClearTime(minute, second);
                 break;
-            case 51: // SHIFT + "
+            case 51: // SHIFT + 
+                if (time != 0) {
+                    i = 0;
+                }
                 time = 1500;
                 minute = time / 60;
                 second = 0;
